c9ec5a79a9d1de89dd75fd38397e7e50ad64b1d7
sofia-fal <sofia-fal@hotmail.com>
2024-09-26 16:25:47 +0200

    création footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 // import { useSelector } from 'react-redux';
 import Header from './components/header'
+import Footer from './components/footer'
 import Home from './pages/home';
 import Signin from './pages/signin';
 
 export default function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
