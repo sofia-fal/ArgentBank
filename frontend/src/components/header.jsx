15f03e0dba525977154e2174a8445d70444eca9a
sofia-fal <sofia-fal@hotmail.com>
2024-09-29 15:28:03 +0200

    création de la page d'accueil

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../style/header.css';
+import '../style/sr.css';
 import Logo from '../assets/argentBankLogo.webp';
 
 function Header() {
