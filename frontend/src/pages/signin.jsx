0c095270864f4e047e66715cabca965fc51223f5
sofia-fal <sofia-fal@hotmail.com>
2024-09-30 15:51:53 +0200

    création de la page de connexion

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Login from '../components/login';
+import '../style/signin.css';
 
 function Signin() {
-  return <div>Sign in</div>;
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <Login />
+      </section>
+    </main>
+  );
 }
 
 export default Signin;
